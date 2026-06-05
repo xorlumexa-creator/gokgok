0304699e679bf6b23427896336b338e24f0223aa
xorlumexa-creator <xorlumexa@gmail.com>
2026-06-05 20:37:24 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import App from "./App.tsx";
 import "./index.css";
 import { registerAppServiceWorker } from "./lib/registerServiceWorker";
 
+// Disable animations on WebView/Android
+if (typeof navigator !== 'undefined' && 
+    (navigator.userAgent.includes('wv') || 
+     navigator.userAgent.includes('Android'))) {
+  document.documentElement.style.setProperty('--animation-duration', '0ms');
+}
+
 registerAppServiceWorker();
 
 createRoot(document.getElementById("root")!).render(<App />);
